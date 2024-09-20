e9d833f9214775cab16b9a7201fe6c5c7f5723ac
Gowtham Raju <gowthamraju239@gmail.com>
2024-09-20 14:32:35 +0530

    working with the buttons

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function() {
-    sketchBoard(16)
+    sketchBoard(16);
+    const inputButton = document.getElementById("select");
+    inputButton.addEventListener("click", () => {
+        let size = getSize();
+        if (size) { // Check if size is valid before calling sketchBoard
+            sketchBoard(size);
+        }
+    });
 });
 
 function sketchBoard(size) {
@@ -10,7 +19,38 @@ function sketchBoard(size) {
     let numOfDivs = size * size;
     for( let i=0; i<numOfDivs; i++) {
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", colorDiv);
         board.insertAdjacentElement("beforeend", div);  
     }
 }
+
+function getSize() {
+    let input = prompt("enter a number between 1 to 100");
+    let message = document.querySelector(".message");
+    if(input == "") {
+        message.innerHTML = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Provide a number between 1 and 100";
+    } else {
+        message.innerHTML = "Let's play";
+        return input;
+    }
+}
+
+function colorDiv() {
+    if(color == "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = "black";
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", () => {
+    let divs = document.querySelectorAll(".sketch-board div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+})
